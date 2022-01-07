a985a6490ea12ff1343b06b01eba51f80d48b11d
KeertanaOfficial <97265053+KeertanaOfficial@users.noreply.github.com>
2022-01-06 20:35:28 -0800

    Amazon app initial setup
    Amazon app initial setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDgMzczkppMG0bcLsp3fwhGcjpKaT67HX4",
+    authDomain: "challenge-396c2.firebaseapp.com",
+    projectId: "challenge-396c2",
+    storageBucket: "challenge-396c2.appspot.com",
+    messagingSenderId: "366443790780",
+    appId: "1:366443790780:web:fac0e003fffa04efa2abb9",
+    measurementId: "G-F29LRCCCP4"
+  };
